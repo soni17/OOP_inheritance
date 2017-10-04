b5084af7fc43b2e85d12e877fdabb4dc47696899
soni17 <elb004@gmail.com>
2017-10-04 17:23:03 -0400

    completed quote collector exercise

---
+++ part2/quote_collector.rb
@@ -0,0 +1,39 @@
+require './multilinguist/multilinguist.rb'
+
+class QuoteCollector < Multilinguist
+
+  def initialize()
+    @favQuotes = [
+      "Work hard to get ahead, work smart to be the head",
+      "Good things come to those who wait, better things come to those who go and get it",
+      "A journey of one thousands miles starts with one step.",
+      "We may encounter many defeats but we must not be defeated."
+    ]
+    super
+  end
+
+  def add_quote(quote)
+    @favQuotes << quote.to_s
+  end
+
+  def say_random_quote()
+    listLength = @favQuotes.length
+    randNum = rand(listLength)
+    quote = @favQuotes[randNum]
+    quote = quote.to_s
+    return "#{say_in_local_language(quote)}"
+  end
+
+end
+
+john = QuoteCollector.new()
+john.add_quote("This is a random quote entered just for testign purposes.")
+puts john.say_random_quote()
+puts john.say_random_quote()
+john.travel_to("Italy")
+puts john.say_random_quote()
+puts john.say_random_quote()
+john.travel_to("Spain")
+puts john.say_random_quote()
+john.travel_to("Albania")
+puts john.say_random_quote()
