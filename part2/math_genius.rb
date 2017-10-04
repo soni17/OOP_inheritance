165c38ea13979169ee72d5ae56c1ca175a354177
soni17 <elb004@gmail.com>
2017-10-04 16:11:17 -0400

    created report_total method

--- part2/math_genius.rb
+++ part2/math_genius.rb
@@ -2,6 +2,12 @@ require './multilinguist/multilinguist.rb'
 
 class MathGenius < Multilinguist
 
-
+  def report_total(list)
+    total = 0
+    list.each do |num|
+      total = total + num
+    end
+    return "#{say_in_local_language("The total is: ")} #{total}"
+  end
 
 end
