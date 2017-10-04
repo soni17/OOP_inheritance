d263fdbe33924a8c4aa57b7deb91964a65ee91cb
soni17 <elb004@gmail.com>
2017-10-04 16:15:49 -0400

    instantiated class and tested methods

--- part2/math_genius.rb
+++ part2/math_genius.rb
@@ -11,3 +11,16 @@ class MathGenius < Multilinguist
   end
 
 end
+
+me = MathGenius.new
+
+puts me.report_total([23,45,676,34,5778,4,23,5465])
+puts " "
+
+me.travel_to("Italy")
+puts me.report_total([6,3,6,68,455,4,467,57,4,534])
+puts " "
+
+me.travel_to("Albania")
+puts me.report_total([324,245,6,343647,686545])
+puts " "
